371622fa56f9ce0d87a2f0fd54476acb84da56bc
hiiamabat <hiiamabat@gmail.com>
2024-04-02 15:37:04 -0600

    Initial file upload

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+  {
+    name: "Strawberry Boba",
+    ingredients: ["tea", "strawberry", "milk", "boba"],
+    image: "/images/strawberry-boba.png",
+    id: 0,
+    price: 8,
+    emoji: "🍓",
+  },
+  {
+    name: "Graham Cracker Boba",
+    ingredients: ["tea", "whipped cream", "graham cracker", "boba"],
+    image: "/images/graham-boba.png",
+    price: 9,
+    emoji: "🧋",
+    id: 1,
+  },
+  {
+    name: "Purple Taro Boba",
+    ingredients: ["tea, purple, taro, milk, boba"],
+    image: "/images/taro-boba.png",
+    price: 7,
+    emoji: "💜",
+    id: 2,
+  },
+];
