68e49ad629aba4cea09eb4291b751a7666c32954
rtdevcraft <hiiamabat@gmail.com>
2025-02-02 22:08:32 -0700

    added vite.config.js to try to fix image issue

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+  build: {
+    rollupOptions: {
+      external: [
+        'images/strawberry-boba.png',
+        'images/graham-boba.png',
+        'images/taro-boba.png',
+      ],
+    },
+  },
+}
