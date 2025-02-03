4fe9281876d88a2b435bd72df42842f1b38d005d
rtdevcraft <hiiamabat@gmail.com>
2025-02-02 21:51:52 -0700

    structured everything with vite trying to fix image issue on deploy to netlify

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import strawberryBoba from './images/strawberry-boba.png'
+import grahamBoba from './images/graham-boba.png'
+import taroBoba from './images/taro-boba.png'
+
+export const menuArray = [
+  {
+    name: 'Strawberry Boba',
+    ingredients: ['tea', 'strawberry', 'milk', 'boba'],
+    image: strawberryBoba,
+    id: 0,
+    price: 8,
+    emoji: '🍓',
+  },
+  {
+    name: 'Graham Cracker Boba',
+    ingredients: ['tea', 'whipped cream', 'graham cracker', 'boba'],
+    image: grahamBoba,
+    price: 9,
+    emoji: '🧋',
+    id: 1,
+  },
+  {
+    name: 'Purple Taro Boba',
+    ingredients: ['tea, purple, taro, milk, boba'],
+    image: taroBoba,
+    price: 7,
+    emoji: '💜',
+    id: 2,
+  },
+]
